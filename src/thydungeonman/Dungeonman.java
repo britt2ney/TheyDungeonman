12a8cc4b02d7b953fe85e057e94d79396838d602
Brittney Schultz <britt2ney@gmail.com>
2018-05-03 10:48:45 -0500

    Second commit.

--- src/thydungeonman/Dungeonman.java
+++ src/thydungeonman/Dungeonman.java
@@ -52,6 +52,119 @@ public class Dungeonman {
         return location;
     }
     
+    public void atMain(String input){
+        switch(input.toLowerCase()){
+            case "help":
+                goMain();
+                break;
+            case "go north":
+                goNorth();
+                break;
+            case "go south":
+                goSouth();
+                break;
+            case "go dennis":
+                goDennis();
+                break;
+            case "get flask":
+               System.out.print("\nYe cannot get the FLASK. It is firmly bolted "
+                    + "to a wall which is bolted to the rest of the dungeon "
+                    + "which is probably bolted to a castle. Never you mind.");
+               break;
+            case "get scroll":
+                System.out.print("\nYe takes the scroll and reads of "
+                        + "it. It doth say:\nBEWARE, READER OF THE "
+                        + "SCROLL, DANGER AWAITS TO THE-\n\n The "
+                        + "SCROLL disappears in thy hands with ye "
+                        + "olde ZAP!");
+                break;
+            default:
+                System.out.print(badInput());
+        }
+    }
+    
+    public void atNorth(String input){
+        switch(input.toLowerCase()){
+            case "help":
+                goNorth();
+                break;
+            case "go south":
+                goMain();
+                break;
+            case "get rope":
+                System.out.print("\nYou attempt to take ye ROPE but "
+                        + "alas it is enchanted! It glows a mustard "
+                        + "red and smells like a public privy. The "
+                        + "ROPE wraps round your neck and hangs you "
+                        + "from parapets. With your last breath, "
+                        + "you wonder what parapets are. GAME OVER\n");
+                setAlive(false);
+                break;
+            default:
+                System.out.print(badInput());
+        }
+    }
+    
+    public void atSouth(String input){
+        switch(input.toLowerCase()){
+            case "help":
+                goSouth();
+                break;
+            case "go north":
+                goMain();
+                break;
+            case "get trinket":
+                if(hasTrinket()){
+                   System.out.print("\nSigh. The trinket is in thou pouchel. Recallest thou?");
+                }
+                else{
+                    System.out.print("\nYe getsts yon TRINKET and discover "
+                        + "it to be a bauble. You rejoice at your "
+                        + "good fortune. You shove the TRINKET in "
+                        + "your pouchel. It kinda hurts.");
+                    setTrinket(true);
+                }
+                break;
+            default:
+                System.out.print(badInput());
+        }
+    }
+    
+    public void atDennis(String input){
+        switch(input.toLowerCase()){
+            case "help":
+                goDennis();
+                break;
+            case "not dennis":
+                goMain();
+                break;
+            case "talk dennis":
+                System.out.print("\nYou engage Dennis in leisurely "
+                        + "discussion. Ye learns that his jimberjam "
+                        + "was purchased on sale at a discount market "
+                        + "and that he enjoys pacing about nervously. "
+                        + "You become bored and begin thinking about parapets.");
+                break;
+            case "give trinket":
+                if(hasTrinket()){
+                    System.out.print("\nA novel idea! You givst the "
+                        + "TRINKET to Dennis and he happily agrees "
+                        + "to tell you what parapets are. With this "
+                        + "new knowledge, ye escapes from yon "
+                        + "dungeon in order to search for new "
+                        + "dungeons and to remain... \n\nTHY DUNGEONMAN!! "
+                        + "\nYou hath won! Congraturation!!\n");
+                    setAlive(false);
+                }
+                else{
+                    System.out.print("Thou don'tst have a trinket to give. Go back to your tiny life.");
+                }                
+                break;
+            default:
+               System.out.print(badInput()); 
+        }
+    }
+    
     public void goMain(){
         System.out.print("\nYe find yeself in yon dungeon. Ye see a SCROLL. "
                 + "Behind ye scroll is a FLASK. Obvious exits are NORTH, "
@@ -81,6 +194,12 @@ public class Dungeonman {
     
     public void clearScreen(){
         System.out.print("\033[H\033[2J");
+    }
+    
+    public void prompt(){
+        if(isAlive()){
+            System.out.print("\n\nWhat wouldst thou deau?\n>");
+        }
     }
     
     public String badLocation(){
--- src/thydungeonman/Game.java
+++ src/thydungeonman/Game.java
@@ -21,117 +21,28 @@ public class Game {
         String input;
         Dungeonman dude = new Dungeonman();
         dude.goMain();
-        System.out.print("\n\nWhat wouldst thou deau?\n>");
+        dude.prompt();
         
         do{
             input = keyboard.nextLine();
             dude.clearScreen();
             switch(dude.getLocation()){
                 case "main":
-                    switch(input.toLowerCase()){
-                        case "help":
-                            dude.goMain();
-                            break;
-                        case "go north":
-                            dude.goNorth();
-                            break;
-                        case "go south":
-                            dude.goSouth();
-                            break;
-                        case "go dennis":
-                            dude.goDennis();
-                            break;
-                        case "get flask":
-                           System.out.print("\nYe cannot get the FLASK. It is firmly bolted "
-                                + "to a wall which is bolted to the rest of the dungeon "
-                                + "which is probably bolted to a castle. Never you mind.");
-                           break;
-                        case "get scroll":
-                            System.out.print("\nYe takes the scroll and reads of "
-                                    + "it. It doth say:\nBEWARE, READER OF THE "
-                                    + "SCROLL, DANGER AWAITS TO THE-\n\n The "
-                                    + "SCROLL disappears in thy hands with ye "
-                                    + "olde ZAP!");
-                            break;
-                        default:
-                            System.out.print(dude.badInput());
-                    }
+                    dude.atMain(input);
                     break;
                 case "north":
-                    switch(input.toLowerCase()){
-                        case "help":
-                            dude.goNorth();
-                            break;
-                        case "go south":
-                            dude.goMain();
-                            break;
-                        case "get rope":
-                            System.out.print("\nYou attempt to take ye ROPE but "
-                                    + "alas it is enchanted! It glows a mustard "
-                                    + "red and smells like a public privy. The "
-                                    + "ROPE wraps round your neck and hangs you "
-                                    + "from parapets. With your last breath, "
-                                    + "you wonder what parapets are. GAME OVER");
-                            dude.setAlive(false);
-                            break;
-                        default:
-                            System.out.print(dude.badInput());
-                    }
+                    dude.atNorth(input);
                     break;
                 case "south":
-                    switch(input.toLowerCase()){
-                        case "help":
-                            dude.goSouth();
-                            break;
-                        case "go north":
-                            dude.goMain();
-                            break;
-                        case "get trinket":
-                            if(dude.hasTrinket()){
-                               System.out.print("\nSigh. The trinket is in thou pouchel. Recallest thou?");
-                            }
-                            else{
-                                System.out.print("\nYe getsts yon TRINKET and discover "
-                                    + "it to be a bauble. You rejoice at your "
-                                    + "good fortune. You shove the TRINKET in "
-                                    + "your pouchel. It kinda hurts.");
-                            }
-                            break;
-                        default:
-                            System.out.print(dude.badInput());
-                    }
+                    dude.atSouth(input);
                     break;
                 case "dennis":
-                    switch(input.toLowerCase()){
-                        case "help":
-                            dude.goDennis();
-                            break;
-                        case "not dennis":
-                            dude.goMain();
-                            break;
-                        case "talk dennis":
-                            System.out.print("\nYou engage Dennis in leisurely "
-                                    + "discussion. Ye learns that his jimberjam "
-                                    + "was purchased on sale at a discount market "
-                                    + "and that he enjoys pacing about nervously. "
-                                    + "You become bored and begin thinking about parapets.");
-                            break;
-                        case "give trinket":
-                            System.out.print("\nA novel idea! You givst the "
-                                    + "TRINKET to Dennis and he happily agrees "
-                                    + "to tell you what parapets are. With this "
-                                    + "new knowledge, ye escapes from yon "
-                                    + "dungeon in order to search for new "
-                                    + "dungeons and to remain... \n\nTHY DUNGEONMAN!! "
-                                    + "\nYou hath won! Congraturation!!");
-                            dude.setAlive(false);
-                            break;
-                    }
+                    dude.atDennis(input);
                     break;
                 default:
                     System.out.print(dude.badInput());
-            }        
-            System.out.print("\n\nWhat wouldst thou deau?\n>");
+            }
+            dude.prompt();
         }while(dude.isAlive());
     }
 }
